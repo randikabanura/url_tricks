77b8550d987488274dbfd2791823b3800cbc85cb
Banura Randika <randika.banura@gmail.com>
2021-10-22 11:59:00 +0530

    python setup file update add

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = 'URL Manipulations that is widely used'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="url_tricks",
     version=VERSION,
     author="Banura Randika Perera",
     author_email="<randika.banura@gmail.com>",
