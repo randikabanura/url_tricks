5f016001f2f8f75c538bddac3c0ac7143cba3582
Banura Randika <randika.banura@gmail.com>
2021-10-30 11:59:00 +0530

    google url trick update add

--- python/url_tricks/google.py
+++ python/url_tricks/google.py
@@ -19,15 +19,29 @@ class GoogleUrlTricks:
         Creates a new instance of GoogleUrlTricks
         """
 
-    def downloadable_drive_link(self, url: str):
+    def downloadable_drive_url(self, url: str):
         """
         Gives a downloadable url of a shared link in Google Drive
-        :param url: Should a string and the link of a shared resource in google drive
+        :param url: Should be a string and the link of a shared resource in google drive
         :return: if successful will return downloadable url, otherwise None
         """
 
         try:
             resource_id = url.split("/")[5]
             return f"https://drive.google.com/uc?id={resource_id}"
+        except (RuntimeError, IndexError):
+            return None
+
+    def previewable_docs_url(self, url: str):
+        """
+        Gives a previewable url form the docs url
+        :param url: Should be a string and the url of a shared resource in google docs
+        :return: if successful will return previewable url, otherwise None
+        """
+
+        try:
+            split_url = url.split("/")
+            main_url = "/".join(split_url[:6])
+            return f"{main_url}/preview"
         except (RuntimeError, IndexError):
             return None
